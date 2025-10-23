03078c25b7fcbf1cfcc14841b5fc9ec79271f5e1
Suvajit <suvajitmondal2019@gmail.com>
2025-10-23 09:08:25 +0530

    add about us page

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import "../scss/aboutUs.css";
 
-const AboutUsPage = () => {
+const developers = [
+  {
+    name: "Souvik Mondal",
+    role: "Senior Developer",
+    img: "",
+    about: "",
+  },
+  {
+    name: "Apoorva Singh",
+    role: "Junior Programmer",
+    img: "",
+    about: "",
+  },
+  {
+    name: "Suvajit Mondal",
+    role: "Junior Programmer",
+    img: "",
+    about: "Enjoys integrating frontend and backend to deliver complete web solutions.",
+  },
+  {
+    name: "Bimam Maity",
+    role: "Junior Programmer",
+    img: "",
+    about: "Passionate about design systems and creating seamless user experiences.",
+  },
+];
+
+const AboutUs = () => {
   return (
-    <div>
-      <h1>Aboutus</h1>
+    <div className="about-container">
+      <h1 className="about-title">About Us</h1>
+      <div className="developer-list">
+        {developers.map((dev, index) => (
+          <div key={index} className="developer-card">
+            <img src={dev.img} alt={dev.name} className="developer-img" />
+            <h3>{dev.name}</h3>
+            <p className="role">{dev.role}</p>
+            <p className="about-text">{dev.about}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutUsPage;
+export default AboutUs;
